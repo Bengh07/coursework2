e8c3222bd51a7946b1b3e94831466a8301294bae
Bengh07 <bengantundubantumponga@gmail.com>
2025-03-19 16:36:37 +0300

    commit #11
    in the new version online 1 and 2 we have imported the pachage1 and 2. on the nv on line 6 up to 9 we have instances for each class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import package1.class1;
+import package2.class2;
 public class Main {
     public static void main(String[] args) {
 
+    class1 cl1=new class1("bengh richard",24,"kalungu road soya","Male");
+    cl1.getpatientInfo();
+    class2 cl2=new class2(234);
+    cl2.getpatientRoomNumber();
     }
 }
