3ee44b461a7efa579476837dd95a3808a4a1b4c5
Bengh07 <bengantundubantumponga@gmail.com>
2025-03-19 15:37:44 +0300

    commit #5
    in the new version online 2 up to 14 , we have declared four variables and created a constructor to initialize all variables that has been created

--- module1/src/class1.java
+++ module1/src/class1.java
@@ -1,2 +1,16 @@
 public class class1 {
+    String patientname;
+    int  patientID;
+    String patientadress;
+    String patientgender;
+
+    public class1(String patientname, int patientID, String patientadress, String patientgender) {
+        this.patientname = patientname;
+        this.patientID = patientID;
+        this.patientadress = patientadress;
+        this.patientgender = patientgender;
+    }
+
+    
 }
+
