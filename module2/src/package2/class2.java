ac5f88b1181efee39e137f9c2dac96982fdb017c
Bengh07 <bengantundubantumponga@gmail.com>
2025-03-19 16:22:32 +0300

    commit #9
    in the nv on line 1 up to 14 , we have created a new class, declared a new variable , created a constructor to initialize the variable that has been created and finishing by creating a new method to get the variable

---
+++ module2/src/package2/class2.java
@@ -0,0 +1,14 @@
+package package2;
+
+public class class2 {
+    int roomNumber;
+
+    public class2(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void getpatientRoomNumber(){
+        System.out.println("the patient Room Number is"+""+roomNumber);
+    }
+
+}
